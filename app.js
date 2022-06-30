0d73b154aa1ca85a074f54de5d645f97060bf387
Sourav Bain <souravbain8@gmail.com>
2022-06-30 20:20:46 +0530

    first one

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.post("/", function (req, res) {
     res.redirect("/");
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running at port 3000");
 })
